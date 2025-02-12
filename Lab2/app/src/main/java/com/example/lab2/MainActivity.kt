1c90647beb974389dc315516afe2fa9f34de450d
Rafael Abreu <abreurafa22@gmail.com>
2025-02-11 21:31:21 -0500

    Lab2

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lab2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+lateinit var emails: List<Email>
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val view = findViewById<RecyclerView>(R.id.emailsRv)
+        emails = EmailFetcher.getEmails()
+
+        val adapter = EmailAdapter(emails)
+
+        view.adapter = adapter
+        // Set layout manager to position the items
+
+        view.layoutManager =  LinearLayoutManager(this)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
